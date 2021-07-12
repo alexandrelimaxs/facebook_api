4b5f878387459f7e24f9da3e8fe1b65b90b7ff72
Alexandre Colmenero <alexandrelimaxs@gmail.com>
2021-07-12 17:12:51 -0300

    versão finalizada e separada por rede

--- main/Facebook_api.py
+++ main/Facebook_api.py
@@ -8,8 +8,6 @@ token = "EAADsWrQWaGABAGyLVcZB0KK82UrXb2L1buoAkOdu7NZAw7eabJ6jDAeSVj69e8HK5EwUQa
 
 # --------------------------------------------------
 
-
-
 def api_request(url):
     res = requests.get(url)
     return json.loads(res.text)
@@ -59,48 +57,37 @@ def get_database(idlist):
     for loopid in idlist:       
         thisid = []
 
-        loopdata = api_request("https://graph.facebook.com/v11.0/"+loopid+"?fields=likes.summary(true).limit(0)%2Ccomments.summary(total_count).limit(0)%2Cshares%2Cpermalink_url%2Cid%2Cmessage%2Ccreated_time&access_token="+token)
-        loopmetrics = api_request("https://graph.facebook.com/v11.0/"+loopid+"/insights?metric=post_reactions_by_type_total%2Cpost_impressions%2Cpost_impressions_unique%2Cpost_impressions_organic%2Cpost_impressions_organic_unique%2Cpost_impressions_paid%2Cpost_impressions_paid_unique%2Cpost_video_views_10s%2Cpost_video_views_10s_unique&access_token="+token)
+        loopdata = api_request("https://graph.facebook.com/v11.0/"+loopid+"?fields=likes.summary(true).limit(0)%2Ccomments.summary(total_count).limit(0)%2Cshares%2Cpermalink_url%2Cid%2Cmessage%2Ccreated_time%2Cfull_picture&access_token="+token)
+        loopmetrics = api_request("https://graph.facebook.com/v11.0/"+loopid+"/insights?metric=post_impressions%2Cpost_impressions_paid%2Cpost_impressions_organic%2Cpost_impressions_unique%2Cpost_impressions_paid_unique%2Cpost_impressions_organic_unique%2Cpost_video_views_10s%2Cpost_video_views_10s_paid&access_token="+token)
         
         
         if "message" in loopdata:
+
             thisid.append(loopdata['created_time']) if "created_time" in loopdata else thisid.append(0)
             thisid.append(loopdata['message']) if "message" in loopdata else thisid.append(0)
-            thisid.append(loopmetrics['data'][1]['values'][0]['value']) #impressao
-            thisid.append(loopmetrics['data'][3]['values'][0]['value']) #impressao organica
-            thisid.append(loopmetrics['data'][5]['values'][0]['value']) #impressao pago
-            thisid.append(loopmetrics['data'][2]['values'][0]['value']) #alcance
-            thisid.append(loopmetrics['data'][4]['values'][0]['value']) #alcance organica
-            thisid.append(loopmetrics['data'][6]['values'][0]['value']) #alcance pago
-            thisid.append(loopmetrics['data'][7]['values'][0]['value']) #visualizações 10s
-            thisid.append(loopmetrics['data'][8]['values'][0]['value']) #visualizações 10s unicas
-            thisid.append(loopmetrics['data'][9]['values'][0]['value']) #impressao viral
-            thisid.append(loopmetrics['data'][10]['values'][0]['value']) #alcance viral
-            thisid.append(loopmetrics['data'][11]['values'][0]['value']) #impressao  não viral
-            thisid.append(loopmetrics['data'][12]['values'][0]['value']) #alcance não viral
+            thisid.append(loopdata['likes']['summary']['total_count']) if "likes" in loopdata else thisid.append(0)
             thisid.append(loopdata['comments']['summary']['total_count']) if "comments" in loopdata else thisid.append(0)
             thisid.append(loopdata['shares']['count']) if "shares" in loopdata else thisid.append(0)
-            thisid.append(loopdata['likes']['summary']['total_count']) if "likes" in loopdata else thisid.append(0)
-            thisid.append(loopmetrics['data'][0]['values'][0]['value']['like']) if "like" in loopmetrics['data'][0]['values'][0]['value'] else thisid.append(0)
-            thisid.append(loopmetrics['data'][0]['values'][0]['value']['love']) if "love" in loopmetrics['data'][0]['values'][0]['value'] else thisid.append(0)
-            thisid.append(loopmetrics['data'][0]['values'][0]['value']['wow']) if "wow" in loopmetrics['data'][0]['values'][0]['value'] else thisid.append(0)
-            thisid.append(loopmetrics['data'][0]['values'][0]['value']['haha']) if "haha" in loopmetrics['data'][0]['values'][0]['value'] else thisid.append(0)
-            thisid.append(loopmetrics['data'][0]['values'][0]['value']['sorry']) if "sorry" in loopmetrics['data'][0]['values'][0]['value'] else thisid.append(0)
-            thisid.append(loopmetrics['data'][0]['values'][0]['value']['anger']) if "anger" in loopmetrics['data'][0]['values'][0]['value'] else thisid.append(0)
+
+            for loop in range(8):
+
+                thisid.append(loopmetrics['data'][loop]['values'][0]['value']) 
+
             thisid.append(loopdata['id']) if "id" in loopdata else thisid.append(0)
             thisid.append(loopdata['permalink_url']) if "permalink_url" in loopdata else thisid.append(0)
+            thisid.append(loopdata['full_picture']) if "full_picture" in loopdata else thisid.append(0)
 
             tabela.append(thisid)
 
 
     #retorna a tabela em dataframe
-    return pd.DataFrame(tabela, columns=["Data","Texto","Impressão","Impressão Orgânica","Impressão Paga","Alcance","Alcance Orgânica","Alcance Pago","Visualizações","Visualizações Únicas","Impressão Viral","Alcance Viral","Impressão Não Viral","Alcance Não Viral","Comentários","Shares","Reações","Likes","Amei","Wow","Haha","Triste","Grr","Id","Link"])   
+    return pd.DataFrame(tabela, columns=["Data","Texto","Reações","Comentários","Compartilhamentos","Impressão","Impressão Paga","Impressão Orgânica","Alcance","Alcance Pago","Alcance Orgânico","Visualizações 10s Total","Visualizações 10s Pagas","ID","Link","Link da Imagem"])   
 
 def export_excel(database,nome):
     output = nome + ".xlsx"
     database.to_excel(output)
     return print('Arquivo exportado com o nome {}'.format(output))  
 
-tabela = get_database(get_ids('28/06/2021','01/07/2021'))
-export_excel(tabela,'Junho - 2021')
+tabela = get_database(get_ids('01/06/2021','01/07/2021'))
+export_excel(tabela,'Facebook - Junho 21')
 
--- main/Instagram_api.py
+++ main/Instagram_api.py
@@ -12,61 +12,93 @@ def api_request(url):
 
 def date_to_unix(data):
 
-	dataf = data.split('/')
-	dataf = [int(x) for x in dataf]
-	dataf = datetime(dataf[2],dataf[1],dataf[0])
-	return	datetime.timestamp(dataf)
+    dataf = data.split('/')
+    dataf = [int(x) for x in dataf]
+    dataf = datetime(dataf[2],dataf[1],dataf[0])
+    return	datetime.timestamp(dataf)
 
 def filtro_data(database,data_inicial,data_final):
 
-	idlist_filtro = []
+    idlist_filtro = []
 
-	for loop in range(len(database['data'])):
+    for loop in range(len(database['data'])):
 
-		if int(data_inicial) < database['data'][loop]['timestamp'] < int(data_final):
-			
-			idlist_filtro.append(database['data'][loop]['id'])
+        if int(data_inicial) < database['data'][loop]['timestamp'] < int(data_final):
+            
+            idlist_filtro.append(database['data'][loop]['id'])
 
-		elif database['data'][loop]['timestamp'] < data_inicial:
+        elif database['data'][loop]['timestamp'] < data_inicial:
 
-			break
+            break
 
-			
-	return idlist_filtro
+            
+    return idlist_filtro
 
 def get_ids_instagram(inicio, fim):
 
-	data_inicial = date_to_unix(inicio)
-	data_final = date_to_unix(fim)
+    data_inicial = date_to_unix(inicio)
+    data_final = date_to_unix(fim)
 
-	print("Inicial:{}".format(data_inicial))
-	
-	print("Final:{}".format(data_final))
+    idlist = []
+    request_url = "https://graph.facebook.com/v11.0/17841402122853134/media?fields=id%2Ctimestamp&date_format=U&access_token="+token
+    
+    database = api_request(request_url) 							
+
+    while database['data'][-1]['timestamp'] > data_final: 							
+        
+        database = api_request(database['paging']['next'])
+        
+
+    else:
+
+        while database['data'][-1]['timestamp'] > data_inicial:							# enquanto a data do ultimo item do request for posterior ao INICIO do intervalo, coleta IDS
+
+            idlist = idlist + filtro_data(database,data_inicial,data_final)
+            database = api_request(database['paging']['next']) 					# substitui database antiga pela nova
+
+        else:														# se o ultimo item passar o INICIO do intervalo, varrer database buscando os restantes e para o loop
+
+            idlist = idlist + filtro_data(database,data_inicial,data_final)
+            return idlist
+
+def get_database(idlist):
+
+    tabela = []
+
+    for loopid in idlist:
+        
+        thisid = []
+        
+        loopdata = api_request("https://graph.facebook.com/v11.0/"+loopid+"?fields=caption%2Ctimestamp%2Cmedia_product_type%2Cmedia_type%2Clike_count%2Ccomments_count%2Cid%2Cpermalink%2Cmedia_url&access_token="+token)
+        loopmetrics = api_request("https://graph.facebook.com/v11.0/"+loopid+"/insights?metric=reach%2Cimpressions%2Csaved&access_token="+token)
+        
+        thisid.append(loopdata['timestamp']) if "timestamp" in loopdata else thisid.append(0)
+        thisid.append(loopdata['caption']) if "caption" in loopdata else thisid.append(0)
+        # thisid.append(loopdata['media_product_type']) if "media_product_type" in loopdata else thisid.append(0)
+        thisid.append(loopdata['media_type']) if "media_type" in loopdata else thisid.append(0)
+        thisid.append(loopdata['like_count']) if "like_count" in loopdata else thisid.append(0)
+        thisid.append(loopdata['comments_count']) if "comments_count" in loopdata else thisid.append(0)
+        for loop in range(3):
+            thisid.append(loopmetrics['data'][loop]['values'][0]['value'])
+        if loopdata['media_type'] == "VIDEO":
+            videorequest = api_request("https://graph.facebook.com/v11.0/"+loopid+"/insights?metric=video_views&access_token="+token)
+            thisid.append(videorequest['data'][0]['values'][0]['value'])
+        else:
+            thisid.append(0)
+        thisid.append(loopdata['id']) if "id" in loopdata else thisid.append(0)
+        thisid.append(loopdata['permalink']) if "permalink" in loopdata else thisid.append(0)
+        thisid.append(loopdata['media_url']) if "media_url" in loopdata else thisid.append(0)
+        
+        tabela.append(thisid)
+
+    return pd.DataFrame(tabela, columns=["Data","Texto","Tipo do Post","Likes","Comentários","Alcance","Impressões","Salvos","Visualizações","ID","Link","Link da Imagem"])   
+
+def export_excel(database,nome):
+    output = nome + ".xlsx"
+    database.to_excel(output)
+    return print('Arquivo exportado com o nome {}'.format(output))  
+
+database = get_database(get_ids_instagram('01/06/2021','01/07/2021'))
+export_excel(database, "Instagram - Junho 21")
 
 
-	idlist = []
-	request_url = "https://graph.facebook.com/v11.0/17841402122853134/media?fields=id%2Ctimestamp&date_format=U&access_token="+token
-	
-	database = api_request(request_url) 							
-
-	while database['data'][-1]['timestamp'] > data_final: 							
-		print(database['data'][-1]['timestamp'])
-		database = api_request(database['paging']['next'])
-		
-
-	else:
-
-		while database['data'][-1]['timestamp'] > data_inicial:							# enquanto a data do ultimo item do request for posterior ao INICIO do intervalo, coleta IDS
-
-			idlist = idlist + filtro_data(database,data_inicial,data_final)
-			database = api_request(database['paging']['next']) 					# substitui database antiga pela nova
-
-		else:														# se o ultimo item passar o INICIO do intervalo, varrer database buscando os restantes e para o loop
-
-			idlist = idlist + filtro_data(database,data_inicial,data_final)
-			return idlist
-
-database = (get_ids_instagram('01/06/2021','01/07/2021'))
-
-print(database)
-print(len(database))
